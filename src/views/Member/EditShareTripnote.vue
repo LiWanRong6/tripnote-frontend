b809998365033ec7231b5707a76f912785e4e94c
LiWanRong6 <wendy39300@gmail.com>
2022-08-26 14:17:58 +0800

    修改EditShareTripnote.vue

--- src/views/Member/EditShareTripnote.vue
+++ src/views/Member/EditShareTripnote.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="container">
-    <h1>【編輯分享行程】{{ tripnote[0].title }}</h1>
-
-
-
-    <h2 v-for="item in tripnote[0].item" :key="item._id">
+    <h1>【編輯分享行程】{{ tripnote.title }}</h1>
+    <h2 v-for="item in tripnote.item" :key="item._id">
       {{ item.attraction.name }}
       <QuillEditor v-model:content="item.content" :toolbar="toolbar" contentType="html">
       </QuillEditor>
@@ -21,11 +18,23 @@ import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const toolbar = reactive([{ 'header': [1, 2, 3, 4, false] }, 'bold', 'italic', 'underline', { 'align': [] }, { 'color': [] }, { 'background': [] }, 'image', 'link', 'code'])
-const tripnote = reactive([])
+const tripnote = reactive({
+  _id: '',
+  user: '',
+  title: '',
+  image: [],
+  departure: '',
+  return: '',
+  created: '',
+  post: Date.now(),
+  ispost: '',
+  item: [],
+  rows: []
+})
 const shareTripnote = async () => {
   try {
-    tripnote[0].ispost = true
-    await apiAuth.patch('/tripnotes/item/' + route.params.id, tripnote[0])
+    tripnote.ispost = true
+    await apiAuth.patch('/tripnotes/item/' + route.params.id, tripnote)
     Swal.fire({
       icon: 'success',
       title: '成功',
@@ -42,7 +51,16 @@ const shareTripnote = async () => {
 const init = async () => {
   try {
     const { data } = await apiAuth.get('/tripnotes/' + route.params.id)
-    tripnote.push(data.result)
+    tripnote._id = data.result._id
+    tripnote.title = data.result.title
+    tripnote.image = data.result.image
+    tripnote.departure = data.result.date[0].departure
+    tripnote.return = data.result.date[0].return
+    tripnote.created = data.result.date[0].created
+    tripnote.post = data.result.user.post
+    tripnote.user = data.result.user.account
+    tripnote.ispost = data.result.ispost
+    tripnote.item = data.result.item
   } catch (error) {
     Swal.fire({
       icon: 'error',
